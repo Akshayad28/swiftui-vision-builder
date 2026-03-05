68ed9650e469e0fcfe78c05e5ee7ef7c90d74c25
Akshay Deshmukh <95074344+Akshayad28@users.noreply.github.com>
2026-03-05 18:44:34 +0530

    Update code.java

--- code.java
+++ code.java
@@ -1,32 +1,21 @@
-@When("Get sedol values for monitors from excel and TCID={string}")
-public void getSedolCountForMonitorComplete(String TCID) throws SQLException {
+String baseSheetName = monitor.replaceAll("[\\\\/?*\\[\\]]", "_");
 
-    List<String> monitorList = excelWriteClass.getMonitorNamesFromExcel();
-
-    for (String monitor : monitorList) {
-
-        System.out.println("TestCase " + TCID + " execution starts for monitor: " + monitor);
-        testScenario.log("TestCase " + TCID + " execution starts for monitor: " + monitor);
-
-        preProdSedolList =
-                databaseResultSet.getMonitorSedolValue(
-                        Hooks.preprodConn,
-                        monitor.replace("%", "%%")
-                );
-
-        System.out.println("Monitor Name in preprod: " + monitor);
-        testScenario.log("Monitor Name in preprod: " + monitor);
-
-        prodSedolList =
-                databaseResultSet.getMonitorSedolValue(
-                        Hooks.prodConn,
-                        monitor.replace("%", "%%")
-                );
-
-        System.out.println("Monitor Name in Prod: " + monitor);
-        testScenario.log("Monitor Name in Prod: " + monitor);
-
-        // Run comparison step manually
-        compareSedolList(monitor);
-    }
+// Excel sheet name limit = 31 characters
+if (baseSheetName.length() > 25) {
+    baseSheetName = baseSheetName.substring(0, 25);
 }
+
+String sheetName = baseSheetName;
+int counter = 1;
+
+// Ensure unique sheet name
+while (ExcelWriteClass.workbook.getSheet(sheetName) != null) {
+    sheetName = baseSheetName + "_" + counter;
+    counter++;
+}
+
+excelWriteClass.writeSedolRows(
+        sheetName,
+        headers,
+        excelRows
+);
