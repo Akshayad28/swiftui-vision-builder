98b1dd23203b9bea537f9d194f48b0b62eff0944
Akshay Deshmukh <95074344+Akshayad28@users.noreply.github.com>
2026-03-11 16:21:20 +0530

    Remove getCellValue method and add POI dependencies
    Removed getCellValue method and added dependencies for Apache POI.

--- code.java
+++ code.java
@@ -1,35 +1,11 @@
-import org.apache.poi.ss.usermodel.*;
+  <dependency>
+        <groupId>org.apache.poi</groupId>
+        <artifactId>poi</artifactId>
+        <version>5.5.1</version>
+    </dependency>
 
-public String getCellValue(String columnName, int rowNum) {
-
-    Row row = worksheet.getRow(rowNum);
-    Cell cell = row.getCell(colByName.get(columnName));
-
-    if (cell == null) {
-        return "";
-    }
-
-    switch (cell.getCellType()) {
-
-        case NUMERIC:
-            return String.valueOf(cell.getNumericCellValue());
-
-        case STRING:
-            return cell.getStringCellValue();
-
-        case BOOLEAN:
-            return String.valueOf(cell.getBooleanCellValue());
-
-        case BLANK:
-            return "";
-
-        case ERROR:
-            return String.valueOf(cell.getErrorCellValue());
-
-        case FORMULA:
-            return String.valueOf(cell.getNumericCellValue());
-
-        default:
-            return "";
-    }
-}
+    <dependency>
+        <groupId>org.apache.poi</groupId>
+        <artifactId>poi-ooxml</artifactId>
+        <version>5.5.1</version>
+    </dependency>
