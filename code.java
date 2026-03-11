af2db2369d9468c6d5da5a8a99679e9e2edcede8
Akshay Deshmukh <95074344+Akshayad28@users.noreply.github.com>
2026-03-11 18:08:02 +0530

    Update code.java

--- code.java
+++ code.java
@@ -1,43 +1,22 @@
-public static void writeData(Workbook workbook, String scenarioName) {
+@After
+public void closeConnection(Scenario scenario) {
 
     try {
 
-        // Remove spaces & special characters
-        scenarioName = scenarioName.replaceAll("[^a-zA-Z0-9]", "_");
-
-        // Create timestamp
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
-        String timestamp = sdf.format(new Date());
-
-        // Base folder
-        String basePath = System.getProperty("user.dir")
-                + "/src/test/resources/excelfiles/";
-
-        // Scenario folder path
-        String scenarioFolderPath = basePath + scenarioName;
-
-        File scenarioFolder = new File(scenarioFolderPath);
-
-        // Create folder if not exists
-        if (!scenarioFolder.exists()) {
-            scenarioFolder.mkdirs();
+        if (preprodConn != null) {
+            preprodConn.close();
         }
 
-        // Excel file name
-        String fileName = "OracleTestResults_" + timestamp + ".xlsx";
-
-        File file = new File(scenarioFolderPath + "/" + fileName);
-
-        try (FileOutputStream outputStream = new FileOutputStream(file)) {
-
-            workbook.write(outputStream);
-            workbook.close();
-
-            System.out.println("Excel written at: " + file.getAbsolutePath());
-
+        if (prodConn != null) {
+            prodConn.close();
         }
+
+        isDBConnected = false;
+
+        // Pass workbook and scenario name
+        ExcelWriteClass.writeData(workbook, scenario.getName());
 
     } catch (Exception e) {
-        e.printStackTrace();
+        throw new RuntimeException(e);
     }
 }
