d278f7daa90de25f2e98583b02862f2470f61430
Akshay Deshmukh <95074344+Akshayad28@users.noreply.github.com>
2026-03-11 18:05:38 +0530

    Update code.java

--- code.java
+++ code.java
@@ -1,11 +1,43 @@
-  <dependency>
-        <groupId>org.apache.poi</groupId>
-        <artifactId>poi</artifactId>
-        <version>5.5.1</version>
-    </dependency>
+public static void writeData(Workbook workbook, String scenarioName) {
 
-    <dependency>
-        <groupId>org.apache.poi</groupId>
-        <artifactId>poi-ooxml</artifactId>
-        <version>5.5.1</version>
-    </dependency>
+    try {
+
+        // Remove spaces & special characters
+        scenarioName = scenarioName.replaceAll("[^a-zA-Z0-9]", "_");
+
+        // Create timestamp
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        String timestamp = sdf.format(new Date());
+
+        // Base folder
+        String basePath = System.getProperty("user.dir")
+                + "/src/test/resources/excelfiles/";
+
+        // Scenario folder path
+        String scenarioFolderPath = basePath + scenarioName;
+
+        File scenarioFolder = new File(scenarioFolderPath);
+
+        // Create folder if not exists
+        if (!scenarioFolder.exists()) {
+            scenarioFolder.mkdirs();
+        }
+
+        // Excel file name
+        String fileName = "OracleTestResults_" + timestamp + ".xlsx";
+
+        File file = new File(scenarioFolderPath + "/" + fileName);
+
+        try (FileOutputStream outputStream = new FileOutputStream(file)) {
+
+            workbook.write(outputStream);
+            workbook.close();
+
+            System.out.println("Excel written at: " + file.getAbsolutePath());
+
+        }
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
