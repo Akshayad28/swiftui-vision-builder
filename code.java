5598cb67f65e38cddf01e1187430b029583179f0
Akshay Deshmukh <95074344+Akshayad28@users.noreply.github.com>
2026-03-12 20:29:46 +0530

    Update code.java

--- code.java
+++ code.java
@@ -3,3 +3,4 @@ java -cp target/Lho-etl-automation-1.0-SNAPSHOT-shaded.jar org.testng.TestNG tes
 jar tf target/Lho-etl-automation-1.0-SNAPSHOT.jar | findstr TestNG
 
 mvn help:evaluate -Dexpression=project.packaging -q -DforceStdout
+java -cp "target/lho-etl-automation-1.0-SNAPSHOT.jar;target/test-classes;target/classes;target/dependency/*" org.testng.TestNG testng.xml
