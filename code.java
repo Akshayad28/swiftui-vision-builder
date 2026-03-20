7c4096ae9ece84545097ba9e3183c2f839408290
Akshay Deshmukh <95074344+Akshayad28@users.noreply.github.com>
2026-03-20 19:51:51 +0530

    Update code.java

--- code.java
+++ code.java
@@ -35,3 +35,34 @@
         }
     }
 }
+
+
+public static void writeData(XSSFWorkbook workbook, String outlineName) {
+        Date now = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy_HH-mm-ss");
+        String timeDate = sdf.format(now);
+
+        String safeName = outlineName.replaceAll("[^a-zA-Z0-9_-]", "_");
+
+        File file = new File(
+            System.getProperty("user.dir")
+            + "/src/test/resources/excelfiles/"
+            + "OracleTestResults_"
+            + safeName + "_"
+            + timeDate
+            + ".xlsx"
+        );
+
+        try (FileOutputStream outputStream = new FileOutputStream(file)) {
+            workbook.write(outputStream);
+            System.out.println("Excel file written successfully: " + file.getName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                workbook.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
